03064dd04488c47eee288d1cc189f9bd44ccbc07
Huan Liu <546292681lh@gmail.com>
2021-03-28 18:12:47 +0800

    修复不能支持环引用

--- deepclone.js
+++ deepclone.js
@@ -18,26 +18,46 @@ function getType(data) {
 }
 
 function deepClone(data){
-    const type  = getType(data);
-    let tmp
-    if(type === 'array'){
-        tmp = []
-        for(let i =0;i<data.length;i++){
-            tmp.push(deepClone(data[i]))
+    let map = new Map();
+    function baseClone(target){
+        if(!isObject(target)) return target;
+        if(map.get(target)) return map.get(target);
+        let result = Array.isArray(target)?[]:{};
+        map.set(target,result);
+        const keys = Object.keys(target);
+        for(let i = 0;i<keys.length;i++){
+            result[keys[i]] = baseClone(target[keys[i]]);
         }
-        return tmp;
-    }else if(type === 'object'){
-        tmp = {}
-        for(let o in data){
-            tmp[o] = deepClone(data[o])
-        }
-        return tmp;
-    }else{
-        return data;
+        return result;
     }
-
+   return  baseClone(data)
 }
 
+function isObject(obj) {
+    return obj !== null && typeof obj === 'object'
+  }
+
+// function cloneDeep(obj) {
+//     // debugger
+//     let vistedMap = new Map();
+//     function baseClone(target) {
+      
+//       if(!isObject(target)) return target
+  
+//       if(vistedMap.get(target)) return vistedMap.get(target)
+  
+//       let result = Array.isArray(target) ? [] : {}
+  
+//       vistedMap.set(target, result)
+      
+//       const keys = Object.keys(target);
+//       for(let i = 0, len = keys.length; i < len; i++) {
+//         result[keys[i]] = baseClone(target[keys[i]])
+//       }
+//       return result
+//     }
+//     return baseClone(obj)
+//   }
 
 const obj1 ={
     obj_1:{
@@ -49,6 +69,8 @@ const obj1 ={
         console.log('111')
     }
 }
+obj1.self = obj1;
+obj1.self.arr_1[0]=77
 const obj2 = deepClone(obj1);
 obj2.obj_1.a = 3
 obj2.arr_1[0]= 66
